31af40ae7d9d7e88edc25e1caa9ab366bcf1f0a2
Papdi Roland <parraai@INF.ELTE.HU>
2023-09-28 20:14:49 +0200

    Gyak 3

--- laravel-blog/database/seeders/DatabaseSeeder.php
+++ laravel-blog/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Post;
+use \App\Models\Tag;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $users = collect();
+        for ($i=1; $i <= 10; $i++) {
+            $user = User::factory()->create([
+                'email' => 'user' . $i . '@server.com'
+            ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            $users -> add($user);
+        }
+
+        $posts = collect();
+        for ($i=1; $i <= 10; $i++) {
+            $post = Post::factory()->create([
+                'user_id' => $users -> random()
+            ]);
+
+            $posts -> add($post);
+        }
+
+        for ($i=1; $i <= 5; $i++) {
+            $tag = Tag::factory() -> create();
+            // TODO: N-N kapcsolat
+        }
     }
 }
